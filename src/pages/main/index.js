53a530bf05b38958582ebefd9e647335b751bb49
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-17 20:43:36 -0300

    Pagina anterior/proxima

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,29 +5,53 @@ import './styles.css';
 
 export default class Main extends Component {
     state = {
-        photos: []
+        photos: [],
+        page: 1 
     }
 
     componentDidMount() {
         this.loadPhotos();
     }
 
-    loadPhotos = async () => {
-        const response = await api.get('/photos');
+    loadPhotos = async (page = 1) => {
+        const response = await api.get(`/photos/${page}`);
 
-        this.setState({ photos: response.data })
+        this.setState({ photos: response.data, page })
+    }
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadPhotos(pageNumber);
+    }    
+
+    nextPage = () => {
+        const { photos, page } = this.state;
+
+        if(page === photos.length) return;
+
+        const pageNumber = page + 1;
+
+        this.loadPhotos(pageNumber);
     }
 
     render() {
-        const { photos } = this.state;
+        const { photos, page } = this.state;
 
         return (
             <div className="photos-list">
-                {photos.map(photos => (
-                    <article key={photos.id}>
-                        <img src={photos.url} alt={photos.title}></img>
-                    </article>
-                ))}
+                <article key={photos.id}>
+                    <img src={photos.url} alt={photos.title}></img>
+                </article>
+
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === photos.length} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         )
     }
