2786b084688419229d55822b6c5efd5ed6d181d6
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-17 17:40:26 -0300

    Buscando produtos da API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles.css';
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
