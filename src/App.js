d795c4f8935a6b4bb32d359ea5607f0d843378fe
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-17 21:12:21 -0300

    Configurando navegacao

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import Routes from './routes';
 import './styles.css';
 
 import Header from './components/Header';
-import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
