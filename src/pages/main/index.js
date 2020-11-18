de4075ca035c3fd9891dad0c4be86adb00f48e7f
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-18 16:00:23 -0300

    Pesquidsa e estilizaçao

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,6 +5,8 @@ import api from '../../services/api';
 import Header from '../../components/Header';
 
 import Pagination from '@material-ui/lab/Pagination';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { MdClear } from 'react-icons/md';
 
 import './styles.css';
 
@@ -23,7 +25,7 @@ export default class Main extends Component {
     loadPhotos = async (page = 1) => {
         const response = await api.get(`/photos?_start=${(page * 10) - 10}&_limit=10`);
 
-        this.setState({ photos: response.data, page })
+        this.setState({ photos: response.data, page });
     }
 
     handleSubmit = async (event) => {
@@ -31,7 +33,7 @@ export default class Main extends Component {
         if(this.state.value !== '') {
             const response = await api.get(`/photos/${this.state.value}`);
 
-            this.setState({ searchPhoto: response.data })
+            this.setState({ searchPhoto: response.data });
         }
     }
 
@@ -73,33 +75,78 @@ export default class Main extends Component {
 
         if(searchPhoto) {
             return(
-                <div>
-                    <Header />
-                    <article key={searchPhoto.id}>
-                        <img src={searchPhoto.url} alt={searchPhoto.title}></img>
-                    </article>
+                <div className="photos-list">
+                <Header />
+
+                <main id="main">
+                    <div className="search">
+                        <form onSubmit={this.handleSubmit}>
+                            <button className="searchByCode">
+                                <AiOutlineSearch size={20} color="dodgerblue" />
+                            </button>
+
+                            <input type="number" value={this.state.value} placeholder="Procurar por ID" onChange={event => this.setState({ value: event.target.value })} />
+
+                            <button className="clearSearch" onClick={() => {
+                                this.setState({ searchPhoto: null, value: '' });
+                                this.loadPhotos();
+                            }}>
+                                <MdClear size={20} color="#aaaaaa" />
+                            </button>
+                        </form>
+                    </div>
+        
+                    <div className="content">
+                        <article key={searchPhoto.id}>
+                            <img src={searchPhoto.url} alt={searchPhoto.title}></img>
+                            <div className="description">
+                                <strong>ID do Álbum: <p>{searchPhoto.albumId}</p></strong>
+                                <strong>ID da Foto: <p>{searchPhoto.id}</p></strong>
+                                <h1>{searchPhoto.title}</h1>
+                            </div>
+                        </article>
+
+                        <button onClick={() => this.favorites(searchPhoto.id)}>Favoritar</button>
+                    </div>
+                </main>
                 </div>
-            )
+            );
         }
 
         return (
             <div className="photos-list">
                 <Header />
 
-                <main>
+                <main id="main">
                     <div className="search">
                         <form onSubmit={this.handleSubmit}>
-                            <input type="number" value={this.state.value} onChange={event => this.setState({ value: event.target.value })} /> 
+                            <button className="searchByCode">
+                                <AiOutlineSearch size={20} color="dodgerblue" />
+                            </button>
+
+                            <input type="number" value={this.state.value} placeholder="Procurar por ID" onChange={event => this.setState({ value: event.target.value })} />
                         </form>
                     </div>
             
                     {photos.map(photo => {
                         return(
-                            <article key={photo.id}>
-                                <img src={photo.url} alt={photo.title}></img>
+                            <div className="content">
+                                <article key={photo.id}>
+                                    <img src={photo.url} alt={photo.title}></img>
+                                    <div className="description">
+                                        <div className="idS">
+                                            <img src={photo.thumbnailUrl} alt={photo.title}></img>
+                                            <strong>ID do Álbum: <p>{photo.albumId}</p></strong>
+                                            <strong>ID da Foto: <p>{photo.id}</p></strong>
+                                        </div>
+                                        
+                                        <h1>{photo.title}</h1>
+                                    </div>
+                                </article>
+
                                 <button onClick={() => this.favorites(photo.id)}>Favoritar</button>
-                            </article>
-                        )
+                            </div>
+                        );
                     })}
     
                     <div className="actions">
@@ -111,6 +158,6 @@ export default class Main extends Component {
                     </div>
                 </main>
             </div>
-        )
+        );
     }
 }
